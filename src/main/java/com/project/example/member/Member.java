8e9a862d4c500d556080112c5c2dd26758116651
whsung0330 <whsung0330@gmail.com>
2025-06-13 16:31:53 +0900

    mod: member properties getter

--- src/main/java/com/project/example/member/Member.java
+++ src/main/java/com/project/example/member/Member.java
@@ -10,4 +10,16 @@ public class Member {
         this.name = name;
         this.grade = grade;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
 }
