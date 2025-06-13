2bf097151fabaf2ab9e058c5a9e1fb1310cef412
whsung0330 <whsung0330@gmail.com>
2025-06-13 16:31:05 +0900

    feat: member entity

---
+++ src/main/java/com/project/example/member/Grade.java
@@ -0,0 +1,7 @@
+package com.project.example.member;
+
+public enum Grade {
+    BASIC,
+    VIP,
+    ADMIN
+}
---
+++ src/main/java/com/project/example/member/Member.java
@@ -0,0 +1,13 @@
+package com.project.example.member;
+
+public class Member {
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+}
